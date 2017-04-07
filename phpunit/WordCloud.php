4d78a2c4b3edcb7c50703b584b7eeca0f0d697cd
Mangoxylic <erickcho@usc.edu>
2017-04-07 10:02:47 -0700

    Updated white box testing (forgot to push this before)

---
+++ phpunit/WordCloud.php
@@ -0,0 +1,234 @@
+<?php
+require_once 'LibraryController.php';
+
+class WordCloud
+
+
+{
+    
+    /* Builds an array of words with all stop words filtered out. (case-insensitive) */
+    function filter_words($words) {
+        $bad_words = "La,he,she,they,them,they,and,the,I,me,thislyricsisnotforcommercialuse,*******,This,Lyrics,is,NOT,for,Commercial,use";
+        $bad_words = explode(",", $bad_words);
+
+        $filtered_words = array(); // Added this
+
+        foreach ($words as $pos => $word) {
+            if (!in_array(strtolower($word), $bad_words, TRUE)) {
+                $filtered_words[$pos] = $word;
+            }
+        }
+        return $filtered_words;
+    }
+    /* Builds an array of words as keys and their frequency as the value. (case-insensitive) */
+    function word_freq($words) {
+        $frequency_list = array();
+        foreach ($words as $pos => $word) {
+            $word = strtolower($word);
+            if (array_key_exists($word, $frequency_list)) {
+                ++$frequency_list[$word];
+            }
+            else {
+                $frequency_list[$word] = 1;
+            }
+        }
+        return $frequency_list;
+    }
+   
+    /* Builds the word cloud and returns a string containing a div of the word cloud. */
+    function word_cloud($words, $name) {
+        $tags = 0;
+        $cloud = "<div id=\"innerCloud\">";
+        
+        /* This word cloud generation algorithm was taken from the Wikipedia page on "word cloud"
+           with some minor modifications to the implementation */
+        
+        /* Initialize some variables */
+        $fmax = 96; /* Maximum font size */
+        $fmin = 8; /* Minimum font size */
+        $tmin = min($words); /* Frequency lower-bound */
+        $tmax = max($words); /* Frequency upper-bound */
+        foreach ($words as $word => $frequency) {
+        
+            if ($frequency > $tmin) {
+                $font_size = floor(  ( $fmax * ($frequency - $tmin) ) / ( $tmax - $tmin )  );
+                /* Define a color index based on the frequency of the word */
+                $random = rand(1, 3);
+                if($random == 1){
+                    $r = 0; $g = 0; $b = floor( 255 * ($frequency / $tmax) );
+                }else if($random == 2){
+                    $r = 0; $g = floor( 255 * ($frequency / $tmax) ); $b = 0;
+                }else{
+                    $r = floor( 255 * ($frequency / $tmax) ); $g = 0; $b = 0;
+                }   
+               // $r = 0; $g = 0; $b = floor( 255 * ($frequency / $tmax) );
+                $color = '#' . sprintf('%02s', dechex($r)) . sprintf('%02s', dechex($g)) . sprintf('%02s', dechex($b));
+            }
+            else {
+                $font_size = 0;
+            }
+            
+            if ($font_size >= $fmin) {
+                $cloud .= "<a href=\"getSongsForWord.php?artist={$name}&word={$word}\" style=\"font-size: {$font_size}px; color: $color;\">$word</a> ";
+                $tags++;
+            }
+        }
+        
+        $cloud .= "</div>";
+        
+        return array($cloud, $tags);  
+    }
+
+    
+
+    // function getLyricsForArtist($artist) {
+    //     $urlBase = 'http://api.musixmatch.com/ws/1.1/';
+    //     $apiKey = 'c68c8e440ab040696a20c467eb42ddc2';
+    //     // first get track ids
+    //     $query_type = "track.search";
+    //     $artist_name = "?q_artist=" . $artist;
+    //     $query = $urlBase . $query_type . $artist_name . '&apikey=' . $apiKey;
+    //     $response = file_get_contents($query);
+    //     $response = json_decode($response, true);
+    //     $track_list = $response["message"]["body"]["track_list"];
+    //     $track_ids = array( );
+    //     foreach ($track_list as $track) {
+    //         array_push($track_ids, $track["track"]["track_id"]);
+    //     }
+    //     // second get all the long strings of lyrics for each track
+    //     $query_type = "track.lyrics.get";
+    //     $lyrics_list = array( );
+    //     foreach ($track_ids as $id_key => $id) {
+    //         $track_id = "?track_id=" . $id;
+    //         $query = $urlBase . $query_type . $track_id . '&apikey=' . $apiKey;
+    //         $response = file_get_contents($query);
+    //         $response = json_decode($response, true);
+    //         array_push($lyrics_list, $response["message"]["body"]["lyrics"]["lyrics_body"]);
+    //     }
+    //     // turn these long lists of lyrics into a single array that is split by a space delimiter
+    //     //$lyrics = array( );
+    //     $lyrics = "";
+    //     foreach ($lyrics_list as $string_id => $lyric_string) {
+    //         $lyric_array = explode(" ", $lyric_string);
+    //         foreach ($lyric_array as $word_id => $word) {
+    //             $lyrics = $lyrics.$word;
+    //         }
+    //     }
+        
+    //     return $lyrics;
+    // }
+
+    function WordCloudGenerator($author, $limit){
+        
+        $provider = new WordCloud;
+        $library = new LibraryController;
+        $text = $library->combineKeywords($author, $limit);
+        if($text == ""){
+            return "author not found !";
+        }else{
+            $words = str_word_count($text, 1); /* Generate list of words */
+            $word_count = count($words); /* Word count */
+            $unique_words = count( array_unique($words) ); /* Unique word count */
+            $words_filtered = $provider->filter_words($words); /* Filter out stop words from the word list */
+            $word_frequency = $provider->word_freq($words); /* Build a word frequency list */
+            $word_c = $provider->word_cloud($word_frequency, $author); /* Generate a word cloud and get number of tags */
+            $word_cloud = $word_c[0]; /* The word cloud */
+            $tags = $word_c[1]; /* The number of tags in the word cloud*/
+
+            return $word_cloud;
+        }
+        //$text = $provider->getLyricsForArtist($artist_name);
+        
+    }
+
+    // function getSongsByWord($_word, $_artist){
+    //     $urlBase = 'http://api.musixmatch.com/ws/1.1/';
+    //     $apiKey = 'c68c8e440ab040696a20c467eb42ddc2';
+    //     $word_to_trackid_array = array(); // this array maps each word to the array of track_ids that contain that word 
+    //     // first get track ids
+    //     $query_type = "track.search";
+    //     $artist_name = "?q_artist=" . $_artist;
+    //     $query = $urlBase . $query_type . $artist_name . '&apikey=' . $apiKey;
+    //     $response = file_get_contents($query);
+    //     $response = json_decode($response, true);
+    //     $track_list = $response["message"]["body"]["track_list"];
+    //     $track_ids = array( );
+    //     foreach ($track_list as $track) {
+    //         array_push($track_ids, $track["track"]["track_id"]);
+    //     }
+
+    //     // second get all the long strings of lyrics for each track
+    //     $query_type = "track.lyrics.get";
+    //     $lyrics_list = array( );
+    //     foreach ($track_ids as $id_key => $id) {
+    //         $track_id = "?track_id=" . $id;
+    //         $query = $urlBase . $query_type . $track_id . '&apikey=' . $apiKey;
+    //         $response = file_get_contents($query);
+    //         $response = json_decode($response, true);
+    //         $response_lyrics = $response["message"]["body"]["lyrics"]["lyrics_body"];
+    //         $temp_2D_arr = array($response_lyrics, $id); //array storing lyrics at 0 and track_id at 1
+    //         array_push($lyrics_list, $temp_2D_arr); // $lyrics_list stores the 2 element array we created above
+    //     }
+       
+    //     for($x = 0; $x < count($lyrics_list); $x++){ //for each entry in the array $lyrics_list, "lyrics"->"track_id"
+    //         $lyrics_array = explode(" ", $lyrics_list[$x][0]); //explode the lyrics into an array
+    //         for($y = 0; $y < count($lyrics_array); $y++){ //for each word in the array
+
+    //             $word = $lyrics_array[$y]; //get the word
+    //             $word = str_replace(",", "",$word); //remove commas
+    //             $word = str_replace(")", "",$word); //remove bracket
+    //             $word = str_replace("(", "",$word); //remove bracket
+    //             $word = strtolower($word); //convert to lower case
+
+                                
+    //             if(count($word_to_trackid_array) == 0){ // if the $word_to_trackid_array is empty
+    //                 $arr = array(); //create an array for the track_ids 
+    //                 array_push($arr, $lyrics_list[$x][1]); //push the track_id ($lyrics_list[$x][1]) into this array
+    //                 $word_to_trackid_array[$word] = $arr; //the array that we created earlier is the value associated with the key $word
+    //             }else{ //if the $word_to_trackid_array is not empty
+    //                 if(array_key_exists($word, $word_to_trackid_array)){ //if the word already exists as a key
+    //                     $temp = $word_to_trackid_array[$word]; //obtain the word's track_id array
+    //                     if(array_key_exists($lyrics_list[$x][1], $temp)){ // if the track_id already exists in the track_id array 
+    //                          //do nothing
+    //                     }else{ // if the track_id doesn't already exists in the track_id array 
+    //                         array_push($temp, $lyrics_list[$x][1]); //push the track_id into the track_id array
+    //                     }
+    //                 }else{//if the word doesn't already exists as a key
+    //                     $arr = array(); //create an array for the track_ids 
+    //                     array_push($arr, $lyrics_list[$x][1]); //push the track_id ($lyrics_list[$x][1]) into this array
+    //                     $word_to_trackid_array[$word] = $arr; //the array that we created earlier is the value associated with the key $word
+    //                 }
+    //             }
+                
+    //         }
+    //     }
+
+    //     return $word_to_trackid_array[strtolower($_word)]; //return the track_id
+    // }
+    
+    // function getLyricsForSong($_artist, $_track_id, $_word) {
+    //     $urlBase = 'http://api.musixmatch.com/ws/1.1/';
+    //     $apiKey = 'c68c8e440ab040696a20c467eb42ddc2';
+    //     $query_type = "track.lyrics.get";
+    //     $track_id = "?track_id=" . $_track_id;
+    //     $query = $urlBase . $query_type . $track_id . '&apikey=' . $apiKey;
+    //     $response = file_get_contents($query);
+    //     $response = json_decode($response, true);
+    //     $lyrics_body = $response["message"]["body"]["lyrics"]["lyrics_body"];
+    //     return $lyrics_body;
+    // }
+
+    // function getSongByTrackID($_track_id){
+    //     $urlBase = 'http://api.musixmatch.com/ws/1.1/';
+    //     $apiKey = 'c68c8e440ab040696a20c467eb42ddc2';
+    //     $query_type = "track.get";
+    //     $track_id = "?track_id=" . $_track_id;
+    //     $query = $urlBase . $query_type . $track_id . '&apikey=' . $apiKey;
+    //     $response = file_get_contents($query);
+    //     $response = json_decode($response, true);
+    //     $song_title = $response["message"]["body"]["track"]["track_name"];
+    //     return $song_title;
+    // }
+}
+
+?>
---
+++ phpunit/WordCloudTest.php
@@ -0,0 +1,178 @@
+<?php
+require_once 'WordCloud.php';
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test WordCloud.php
+ */
+
+final class WordCloudTest extends TestCase
+{
+	//tests if words are correctly filtered out
+	public function test_filter_words(){
+		//(Arrange) All words should be filtered out except "Hello" and "World"
+		$words = "he,she,they,them,they,and,the,me,thislyricsisnotforcommercialuse,hello,world";
+        $words = explode(",", $words);
+        $hello = "hello,world";
+        $hello = explode(",", $hello);
+
+
+        $allWordsFiltered = "he,she,they,them,they,and,the,me";
+        $allWordsFiltered = explode(",", $allWordsFiltered);
+        $wordsEmpty = ",";
+        $wordsEmpty = explode(",", $wordsEmpty);
+
+        $noWordsFiltered = "tired,world";
+		$noWordsFiltered = explode(",", $noWordsFiltered);
+        $wordsFilled = "tired,world";
+        $wordsFilled = explode(",", $wordsFilled);
+        
+
+        //Act
+        $cloud = new WordCloud();
+        $words = $cloud->filter_words($words);
+        $allWordsFiltered = $cloud->filter_words($allWordsFiltered);
+        $noWordsFiltered = $cloud->filter_words($noWordsFiltered);
+
+
+        //Assert
+        sort($words);
+        sort($hello);
+        
+        //sort($allWordsFiltered);
+        sort($wordsEmpty);
+
+        sort($noWordsFiltered);
+        sort($wordsFilled);
+        
+        $this->assertEquals($words, $hello);
+        $this->assertEquals($noWordsFiltered, $wordsFilled);
+	}
+
+	//tests if word frequency is counted correctly
+	public function test_word_freq(){
+		//Arrange
+		$words = "lol,lol,lol,lmao,HA,ha,ha,ha,jk,jk";
+        $words = explode(",", $words);
+
+
+        //Act
+        $cloud = new WordCloud();
+        $frequency_list = $cloud->word_freq($words);
+
+
+        //Assert
+        //make sure each word is counted correctly
+        $this->assertEquals($frequency_list["lol"], 3);
+        $this->assertEquals($frequency_list["lmao"], 1);
+        $this->assertEquals($frequency_list["ha"], 4);
+        $this->assertEquals($frequency_list["jk"], 2);
+	}
+
+	//Word Cloud is an API so we just need to test if the API call worked or not, not the substance of the API
+	// public function test_word_cloud(){
+	// 	//Arrange
+
+
+ //        //Act
+	// 	$cloud = new WordCloud();
+	// 	$name = "Rihanna";
+ //        $text = $cloud->getLyricsForArtist($name);
+ //        $words = str_word_count($text, 1);
+ //        $word_frequency = $cloud->word_freq($words);
+	// 	$word_c = $cloud->word_cloud($word_frequency, $name);
+	// 	$tags = $word_c[1];
+
+	// 	//Assert
+	// 	//tests if the right number of tags come up 
+	// 	//$this->assertEquals($tags, 14);
+	// 	//tests if the call worked
+	// 	$this->assertLessThan($tags, 0);
+
+	// }
+
+	//this test should check if the API call was successful or not (the job of a remote API client), not what is in those calls
+	// public function test_getLyricsForArtist(){
+	// 	//Arrange
+	// 	$cloud = new WordCloud();
+	// 	$lyrics_content = $cloud->getLyricsForArtist("Rihanna");
+	// 	$lyrics_no_content = $cloud->getLyricsForArtist("asdfadsfadfgag");
+
+	// 	//Assert
+	// 	//make sure lyrics ARE returned for an artist that exists
+	// 	$this->assertLessThan(sizeof($lyrics_content), 0);
+	// 	//make sure NO lyrics returned for an artist that does not exist
+	// 	$this->assertEquals($lyrics_no_content, '');
+
+	// }
+
+	public function test_WordCloudGenerator(){
+
+		//Arrange
+		$cloud = new WordCloud();
+		$name = "madan";
+		// $library = new LibraryController;
+  //       $text = $library->combineKeywords($name, 10);
+		$wordcloud = $cloud->WordCloudGenerator($name, 10);
+		//Act
+        //$text = $cloud->getLyricsForArtist($name);
+  //       $words = str_word_count($text, 1);
+  //       $word_frequency = $cloud->word_freq($words);
+		// $word_c = $cloud->word_cloud($word_frequency, $name);
+		// $tags = $word_c[1];
+
+		//Assert
+		//tests if the right number of tags come up
+		$tags = count($wordcloud); 
+		$this->assertEquals($tags, 1);
+		//tests if the call worked
+		//$this->assertLessThan($tags, 0);
+	}
+
+	// public function test_getLyricsForSong(){
+	// 	//test not needed
+	// }
+
+	// public function test_getSongByTrackID(){
+	// 	//test not needed
+	// }
+
+	// //tests for getSongsByWord
+	// //1) If songs returned by word are accurate. Testing by having a sample word, and seeing if the array that is returned, has a random song that contains that word.
+	// // 2) If word is empty, songs returned are empty
+	// // 3) If word is not alphabetical, songs returned are empty
+	// // 4) If word is not in song, songs returned are empty
+	// public function test_getSongsByWord(){
+	// 	//Arrange
+	// 	$cloud = new WordCloud();
+	// 	$randomSongFound = false;
+	// 	$name = "Rihanna";
+	// 	$word = "thief";
+	// 	$emptyWord = "";
+	// 	$noAlph= "&&&";
+	// 	$noWordInSong = "askjdf";
+		
+	// 	//Act
+	// 	$word = $cloud->getSongsByWord($word,$name);
+	// 	$emptyWord = $cloud->getSongsByWord($emptyWord, $name);
+	// 	$noAlph = $cloud->getSongsByWord($noAlph, $name);
+	// 	$noWordInSong = $cloud->getSongsByWord($noWordInSong, $name);
+
+	// 	//testing to see if array returned from word, has a random song that contains the word.
+	// 	//random song used is disturbia, which does have the word "thief"
+	// 	for($y = 0; $y < count($word); $y++){ 
+	// 		if ($word[$y] == "disturbia")
+	// 			$randomSongFound = true;
+	// 	}
+
+	// 	//Assert
+	// 	$this->assertEquals($randomSongFound, true);
+	// 	$this->assertEquals(sizeof($emptyWord), 0);
+	// 	$this->assertEquals(sizeof($noAlph), 0);
+	// 	$this->assertEquals(sizeof($noWordInSong), 0);
+			
+
+		
+	// }
+
+}
